b05c634c01ff6f819b34fe990b0715ec8ae8a815
Lux <92374048+lucas38460@users.noreply.github.com>
2022-03-15 14:44:44 +0100

    finished connecting

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
 export const pascaltosnake = (entry) => {
     return entry;
+    
 }
--- test/test.js
+++ test/test.js
@@ -2,14 +2,14 @@ import assert from "assert";
 import { pascaltosnake } from "../js/app.js"
 
 
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+// describe('Array', function () {
+//   describe('#indexOf()', function () {
+//     it('should return -1 when the value is not present', function () {
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+// });
+
 
 
 describe('str', function () {
